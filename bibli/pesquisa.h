70aca9a15e01dcb1357b678d2cf98d1a4408207c
Jose Rocha <josemsrocha1044@gmail.com>
2023-04-28 19:45:35 -0300

    corrigido algumas mensagem da interface e alterando como as informações são listadas na tela

--- bibli/pesquisa.h
+++ bibli/pesquisa.h
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-void pesquisarFuncionario() {
+void pesquisarFuncionario()
+{
   FILE *arq;
   char linha[100], *busca, pesquisa[60];
   char nome[60], cpf[12], tel[20], email[30], fil[20], rua[50], num[10],
@@ -17,7 +18,8 @@ void pesquisarFuncionario() {
   printf("----------------------------------------\n\n");
   printf("O QUE DESEJA FAZER: \n");
   scanf("%d", &op);
-  switch (op) {
+  switch (op)
+  {
   case 1:
     strcpy(pesquisa, ":");
     break;
@@ -30,90 +32,106 @@ void pesquisarFuncionario() {
     break;
   }
   arq = fopen("funcionarios/arqfunc.txt", "r");
-  if (arq == NULL) {
+  if (arq == NULL)
+  {
     printf("Erro ao abrir o arquivo!\n");
   }
-  while (fgets(linha, 100, arq) != NULL) {
+  while (fgets(linha, 100, arq) != NULL)
+  {
 
-    if (strstr(linha, pesquisa) != NULL) {
+    if (strstr(linha, pesquisa) != NULL)
+    {
       busca = strtok(linha, ":");
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(nome, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(cpf, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         idade = atoi(busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(tel, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(email, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(fil, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(rua, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(num, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(bairro, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(cid, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(est, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(comp, busca);
       }
 
       printf("-----------------------------------------------------------------"
-             "-----------------------------------------\n\n");
-      printf("| NOME: %s  ", nome);
-      printf("| CPF: %s  ", cpf);
+             "-----------------------------------------\n");
+      printf("| NOME: %s  \n", nome);
+      printf("| CPF: %s  \n", cpf);
       printf("| IDADE: %d \n", idade);
-      printf("| CONTATO: %s  ", tel);
-      printf("| EMAIL: %s  ", email);
+      printf("| CONTATO: %s  \n", tel);
+      printf("| EMAIL: %s  \n", email);
       printf("| FILIAL: %s  \n", fil);
-      printf("| RUA: %s  ", rua);
-      printf("| NUMERO: %s  ", num);
-      printf("| BAIRRO: %s  ", bairro);
+      printf("| RUA: %s  \n", rua);
+      printf("| NUMERO: %s  \n", num);
+      printf("| BAIRRO: %s  \n", bairro);
       printf("| CIDADE: %s  \n", cid);
-      printf("| ESTADO: %s  ", est);
-      printf("| COMPLEMENTO: %s  ", comp);
-      printf("\n\n-------------------------------------------------------------"
+      printf("| ESTADO: %s  \n", est);
+      printf("| COMPLEMENTO: %s  \n", comp);
+      printf("-------------------------------------------------------------"
              "---------------------------------------------");
-      printf("\n\n\n");
+      printf("\n\n");
     }
   }
 
   fclose(arq);
 }
-void pesquisarFilial() {
+void pesquisarFilial()
+{
   FILE *arq;
   char linha[100], *busca;
   char nome[60], rua[50], num[5], bairro[50], cid[40], est[30], comp[100];
@@ -124,7 +142,8 @@ void pesquisarFilial() {
   printf("----------------------------------------\n\n");
   printf("O QUE DESEJA FAZER: \n");
   scanf("%d", &op);
-  switch (op) {
+  switch (op)
+  {
   case 1:
     strcpy(pesquisa, ":");
     break;
@@ -138,63 +157,73 @@ void pesquisarFilial() {
   }
   arq = fopen("filiais/arqfiliais.txt", "r");
 
-  if (arq == NULL) {
+  if (arq == NULL)
+  {
     printf("Erro ao abrir o arquivo!\n");
   }
-  while (fgets(linha, 100, arq) != NULL) {
+  while (fgets(linha, 100, arq) != NULL)
+  {
 
-    if (strstr(linha, pesquisa) != NULL) {
+    if (strstr(linha, pesquisa) != NULL)
+    {
       busca = strtok(linha, ":");
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(nome, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(rua, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(num, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(bairro, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(cid, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(est, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(comp, busca);
       }
       printf("-----------------------------------------------------------------"
-             "-----------------------------------------\n\n");
-      printf("| NOME: %s  ", nome);
-      printf("| RUA: %s  ", rua);
-      printf("| NUMERO: %s  ", num);
+             "-----------------------------------------\n");
+      printf("| NOME: %s  \n", nome);
+      printf("| RUA: %s  \n", rua);
+      printf("| NUMERO: %s  \n", num);
       printf("| BAIRRO: %s  \n", bairro);
-      printf("| CIDADE: %s  ", cid);
-      printf("| ESTADO: %s  ", est);
-      printf("| COMPLEMENTO: %s", comp);
-      printf("\n\n");
-      printf("\n\n-------------------------------------------------------------"
+      printf("| CIDADE: %s  \n", cid);
+      printf("| ESTADO: %s  \n", est);
+      printf("| COMPLEMENTO: %s\n", comp);
+      printf("-------------------------------------------------------------"
              "---------------------------------------------");
-      printf("\n\n\n");
+      printf("\n\n");
     }
   }
 
   fclose(arq);
 }
-void pesquisarProduto() {
+void pesquisarProduto()
+{
   FILE *arq;
   char linha[100], *busca, pesquisa[60], nomearq[30];
   char nome[60], ref[20], cor[30], dep[15], forn[25], fil[20];
@@ -205,7 +234,8 @@ void pesquisarProduto() {
   printf("------------------------------------------------------------\n\n\n");
   printf("QUAL DEPARTAMENTO DESEJA PESQUISAR?\n>>");
   scanf("%d", &opesq);
-  switch (opesq) {
+  switch (opesq)
+  {
   case 1:
     system("cls");
     printf("|--------------------------|\n");
@@ -242,7 +272,8 @@ void pesquisarProduto() {
   printf("----------------------------------------\n\n");
   printf("O QUE DESEJA FAZER: \n");
   scanf("%d", &op);
-  switch (op) {
+  switch (op)
+  {
   case 1:
     strcpy(pesquisa, ":");
     break;
@@ -255,57 +286,66 @@ void pesquisarProduto() {
     break;
   }
   arq = fopen(nomearq, "r");
-  if (arq == NULL) {
+  if (arq == NULL)
+  {
     printf("Erro ao abrir o arquivo!\n");
   }
-  while (fgets(linha, 100, arq) != NULL) {
+  while (fgets(linha, 100, arq) != NULL)
+  {
 
-    if (strstr(linha, pesquisa) != NULL) {
+    if (strstr(linha, pesquisa) != NULL)
+    {
       busca = strtok(linha, ":");
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(nome, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(ref, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(cor, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(dep, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(forn, busca);
         busca = strtok(NULL, ":");
       }
 
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         strcpy(fil, busca);
         busca = strtok(NULL, ":");
       }
-      if (busca != NULL) {
+      if (busca != NULL)
+      {
         qtde = atoi(busca);
       }
       printf("-----------------------------------------------------------------"
              "-----------------------------------------\n\n");
-      printf("| NOME: %s  ", nome);
-      printf("| REFERENCIA: %s  ", ref);
-      printf("| COR: %s  ", cor);
+      printf("| NOME: %s  \n", nome);
+      printf("| REFERENCIA: %s  \n", ref);
+      printf("| COR: %s  \n", cor);
       printf("| DEPARTAMENTO: %s  \n", dep);
-      printf("| FORNECEDOR: %s  ", forn);
-      printf("| FILIAL: %s  ", fil);
-      printf("| ESTOQUE: %d", qtde);
-      printf("\n\n");
-      printf("\n\n-------------------------------------------------------------"
+      printf("| FORNECEDOR: %s  \n", forn);
+      printf("| FILIAL: %s  \n", fil);
+      printf("| ESTOQUE: %d \n", qtde);
+      printf("-------------------------------------------------------------"
              "---------------------------------------------");
-      printf("\n\n\n");
+      printf("\n\n");
     }
   }
 
